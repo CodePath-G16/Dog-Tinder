104799a68f19f0cf235588ded7451902d02892c8
Deki Peltshog <dekipeltshogcodes@gmail.com>
2024-04-20 15:23:14 -0600

    changed welcome font

--- app/src/main/java/com/example/dog_tinder/MainActivity.kt
+++ app/src/main/java/com/example/dog_tinder/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         navigateToWelcomeScreen()
-
     }
 
     private fun navigateToWelcomeScreen() {
